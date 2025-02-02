77c73b218c3a22c8f7f194f088664ff2c070a0ed
Nandhu <nandhukrishna1217@gmail.com>
2025-02-02 19:01:13 +0530

    Added firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   _setNavigationBarColor();
   runApp(const MyApp());
 }
